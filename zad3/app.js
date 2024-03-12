5e033aab2ad4e05260af5bf1377f87095c3ead5a
Kamil Banaszewski <kamil.banaszewski21@wp.pl>
2024-03-12 09:34:17 +0100

    third commit

---
+++ zad3/app.js
@@ -0,0 +1,6 @@
+const student = require('./student.js');
+
+const fullName = student.getStudentFullName();
+const studentId = student.getStudentId();
+
+console.log(`My name is ${fullName}. My student ID is ${studentId}`);
---
+++ zad3/student.js
@@ -0,0 +1,13 @@
+const FULLNAME = "Kamil Banaszewski";
+const STUDENT_ID = 42876;
+
+function getStudentFullName() {
+  return FULLNAME;
+}
+
+function getStudentId() {
+  return STUDENT_ID;
+}
+
+module.exports.getStudentFullName = getStudentFullName;
+module.exports.getStudentId = getStudentId;
